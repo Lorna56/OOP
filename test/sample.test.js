c953182fab7583fec29531d5783d1789b0861a20
Lorna56 <lornanaula0042@gmail.com>
2025-03-27 21:05:14 +0300

    made changes

---
+++ test/sample.test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe("Basic Math Test", function () {
+    it("should return 2 when adding 1 + 1", function () {
+        expect(1 + 1).to.equal(2);
+    });
+});
