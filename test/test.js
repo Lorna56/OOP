9b6f36010ab57f8a283fefe734a1d3f244dad3f1
Lorna56 <lornanaula0042@gmail.com>
2025-03-30 11:12:10 +0300

    Added forth test case

--- test/test.js
+++ test/test.js
@@ -22,4 +22,9 @@
       assert.equal(multiply(3, 3), 9);
     });
   });
+  describe("Testing multiplication on a fourth cycle", () => {
+    it("Should multiply two values", () => {
+      assert.equal(multiply(4, 4), 16);
+    });
+  });
  
